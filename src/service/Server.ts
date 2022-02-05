40711e463805aa31da66a06a673ddb655839a81d
Daniel Sampaio <daniel.sampaio@accurate.com.br>
2022-02-05 19:09:03 -0300

    [FEATURE][SIMPLER_TIMES] Some changes on the way we think communication channels in Dounce

--- src/service/Server.ts
+++ src/service/Server.ts
@@ -1,53 +1,16 @@
 'use strict'
 
-import { ServerDTO } from '../data/ServerDTO'
+import { Channel } from './Channel'
 
-import dgram, { Socket } from 'dgram'
-import { Logger } from '../util/Logger'
+export class Server extends Channel {
 
-export class Server {
+    bind (): void {
 
-    private logger: Logger
+        this.socket.bind(
+            this.data.info.port,
+            this.data.info.address
+        )
 
-    private data: ServerDTO
-
-    private socket!: Socket
-
-    private constructor (data: ServerDTO) {
-        this.data = data
-        this.logger = new Logger('Server')
-    }
-
-    private initSocket () : void {
-
-        this.logger.info('Initializing socket...')
-
-        this.socket = dgram.createSocket(this.data.kind)
-
-        this.socket.on('error', this.data.onError)
-        this.socket.on('listening', this.data.onListening)
-        this.socket.on('message', this.data.onMessage)
-        this.socket.on('close', this.data.onClose)
-
-        this.logger.info('Socket initialized')
-
-    }
-
-    bind () {
-        this.logger.info(`Binding to ${this.data.port}`)
-        this.socket.bind(this.data.port)
-    }
-
-    static create (data: ServerDTO) : Server {
-
-        const server = new Server(data)
-
-        server.logger.info('Starting server %j', data)
-
-        server.initSocket()
-
-        return server
-
-    }
+    }   
 
 }
